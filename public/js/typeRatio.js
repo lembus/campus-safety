ec3ac21b37483a1eece9f33f678ad50b1ef071b2
Mohsen <mohsen.h.abbasi@gmail.com>
2016-11-11 22:28:57 -0700

    tooltip and text added

--- public/js/typeRatio.js
+++ public/js/typeRatio.js
@@ -24,11 +24,13 @@ TypeChart.prototype.init = function(){
     self.svgWidth = self.svgBounds.width - self.margin.left - self.margin.right;
     self.svgHeight = self.svgBounds.height - self.margin.top - self.margin.bottom;
 
+
     //creates svg element within the div
     self.svg = ratioChart.append("svg")
         .attr("width",self.svgWidth)
         .attr("height",self.svgHeight)
 		.attr('style',"border: 3px solid black; display: block; margin: auto;")
+		.attr('id','uni-svg')
 };
 
 TypeChart.prototype.update = function(state,year){
@@ -36,6 +38,26 @@ TypeChart.prototype.update = function(state,year){
 	  
 	var svg = d3.select("#rect-chart").select('svg');
 	
+	var ratioChartScale = d3.scaleLinear()
+		.domain([0, 1])
+		.range([100,  self.svgWidth - 50]);
+		
+	typeNames = [
+		{'name': 'Disciplinary Actions', 'x': 0.85, 'y': 1.03},
+		{'name': 'Hate Crimes', 'x': 0.9, 'y': -0.05},
+		{'name': 'VAWA Offenses', 'x':-0.05, 'y': 1.03},
+		{'name': 'Criminal offenses', 'x':-0.05, 'y':-0.05}
+	]
+	texts = svg.selectAll('text')
+		.data(typeNames)
+		.enter()
+		.append('text')
+        .text(function (d) {return d.name})
+        .attr('x', function (d) {return ratioChartScale(d.x)})
+        .attr('y', function (d) {return ratioChartScale(d.y)})
+        .attr('class', 'typetext');
+		
+		
 	d3.csv("data/" + state + "/crime_types.csv", function (error, crimes) {
 		
 		var ratioChartScale = d3.scaleLinear()
@@ -76,6 +98,12 @@ TypeChart.prototype.update = function(state,year){
 			return parseFloat(e['Institution Size']);
 		});
 		
+		var maxCrime = d3.max(combinedYears,function(e){
+			return parseFloat(e['total']);
+		});
+		
+		maxRatio = maxCrime/maxUniSize;
+		
 		//total crimes by year
 		/*
 		crimes = crimes.filter(function(d) { 
@@ -104,7 +132,34 @@ TypeChart.prototype.update = function(state,year){
 			})
 			.attr('cy', function(d){
 				return ratioChartScale((parseFloat(d['DA']) + parseFloat(d['VW']))/(parseFloat(d['CO'])+parseFloat(d['VW'])+parseFloat(d['HC'])+parseFloat(d['DA'])))
-			});
+			})
+			.attr('id',function(d){
+				return d['Institution name'];
+			})
+			//.attr('fill','#de2d26');
+			.attr('class','uniCircle')
+			.style('opacity',function(d){
+				return (0.5 + parseFloat(d['total'])/parseFloat(d['Institution Size']))/maxRatio
+			})
+			.attr('onmouseover',function(d){
+				return 'unitip(event,"' + d['Institution name'] + '")';
+			})
+			.attr('onmouseout','nunitip()');
 	});
 
 };
+
+function unitip(e,name){
+	var childcircle = document.getElementById("uni-svg").children;
+	var left  = (parseInt(e.clientX) + 10).toString()  + "px";
+    var top  = (parseInt(e.clientY) - 20).toString()  + "px";
+    var div = document.getElementById("tooltipdiv");
+	div.innerHTML = name;
+    div.style.left = left;
+    div.style.top = top;
+	div.style.color = "Green";
+	$("#tooltipdiv").toggle();
+}
+function nunitip(){
+	$("#tooltipdiv").toggle();
+}
