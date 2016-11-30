526c6aaf865683104ccef8016d59dfba0aed7a33
Amir <amir.biglari@utah.edu>
2016-11-30 03:41:14 -0700

    Added the gradient colormap. Added a place holder for the line charts. Aligned the divisins and SVGs nicely.

--- public/js/map.js
+++ public/js/map.js
@@ -21,11 +21,9 @@ MapChart.prototype.init = function(){
     var self = this;
     self.margin = {top: 0, right: 0, bottom: 0, left: 0};
 
-    var width = 0.50 * window.innerWidth;
-    var height = 0.50 * window.innerHeight;
+    var height = 0.4 * window.outerHeight;
 
-    var divmapChart = d3.select("#map-div")
-        .style('width', width + 'px')
+    var divmapChart = d3.select("#mapArea").classed("leftChart",true)
         .style('height',height +'px');
 
     //Gets access to the div element created for this chart from HTML
@@ -35,13 +33,80 @@ MapChart.prototype.init = function(){
 
     //creates svg element within the div
     self.svg = divmapChart.select("svg")
-        .attr("width",self.svgWidth)
-        .attr("height",self.svgHeight)
+        .attr("width",self.svgWidth*0.79)
+        .attr("height",self.svgHeight);
 
     self.svg.append('g')
         .attr("id",'map');
+
+    divmapChart.append('svg')
+        .attr('id','colormap')
+        .attr("width",self.svgWidth*0.19)
+        .attr("height",self.svgHeight);
+
+    var defs = divmapChart.select('#colormap').append("defs");
+
+    var gradient = defs.append("linearGradient")
+        .attr("id", "gradient")
+        .attr("x1", "50%")
+        .attr("x2", "50%")
+        .attr("y1", "100%")
+        .attr("y2", "0%");
+
+    gradient.append("stop")
+        .attr("offset", "0%")
+        .attr("stop-color", "white");
+
+    gradient.append("stop")
+        .attr("offset", "100%")
+        .attr("stop-color", "darkred");
+
+    divmapChart.select("#colormap").append('rect')
+        .attr("x", '30%')
+        .attr("y", '10%')
+        .attr("width", '20%')
+        .attr("height", '80%')
+        .attr('stroke','black')
+        .attr('stroke-width',1)
+        .attr("fill", "url(#gradient)");
+
     self.drawMap();
 
+    var years = [2001];
+    for (i=1;i<14;i++) {
+        years.push(2001+i);
+    }
+    var sliderDivt = d3.select("#slider").classed("leftChart",true);
+    var sliderAxis = d3.select('#slider').select('svg');
+    var maxTick = 98.2;
+    var sliderTicks = sliderAxis.selectAll('rect')
+        .data(years)
+        .enter().append('rect')
+        .attr('x',function(d,i) {
+            return maxTick*i/(years.length-1)+"%";
+        })
+        .attr('y',0)
+        .attr('width',1)
+        .attr('height',7);
+    var maxTick = 98.2;
+    var sliderTicks = sliderAxis.selectAll('text')
+        .data(years)
+        .enter().append('text')
+        .text(function (d) {
+            return d;
+        })
+        .attr('x',function(d,i) {
+            return maxTick*i/(years.length-1)+"%";
+        })
+        .attr('y',20)
+        .attr('text-anchor','middle');
+
+    //Creating the map
+    //var mapChart = new MapChart(lineChart, rectChart);
+    self.update(2001);
+    d3.select("#yearSlider").on("input", function() {
+        self.update(+this.value);
+    });
 };
 
 
@@ -93,6 +158,28 @@ MapChart.prototype.update = function(year) {
         var max = d3.max(cumulativeData, function (d) {
             return d.totalRate
         });
+
+        var colormapSvg = d3.select('#colormap');
+
+        var colormapScale = d3.scaleLinear()
+            .domain([0, max])
+            .range([colormapSvg.attr('height') * 0.8, 0])
+            .nice();
+
+        var colormapAxis = d3.axisRight().scale(colormapScale);
+        colormapSvg.append('g')
+            .attr("transform", "translate(" + colormapSvg.attr('width')*0.5 + "," + colormapSvg.attr('height')*0.099 + ")")
+            .call(colormapAxis);
+
+        colormapSvg.append("g").append('text')
+            .attr("text-anchor", "middle")
+            .attr('transform','rotate(-90,' + colormapSvg.attr('width')*0.9 + ',' + colormapSvg.attr('height')*0.5 + ')')
+            .attr("y", colormapSvg.attr('height')*0.5)
+            .attr("x", colormapSvg.attr('width')*0.9)
+            .attr('stroke','black')
+            .style('font-size','12')
+            .style('stroke-width', '0.1px')
+            .text("#Crimes/#Students");
 
         var colorScale = d3.scaleLinear()
             .domain([0, max])
